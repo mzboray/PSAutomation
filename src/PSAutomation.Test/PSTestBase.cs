cb550fc6f0d234bfe8cfe45b6dbaa7781a194035
Mike Zboray <mzboray@users.noreply.github.com>
2014-08-21 00:22:50 -0700

    use shared runspace in tests

--- src/PSAutomation.Test/PSTestBase.cs
+++ src/PSAutomation.Test/PSTestBase.cs
@@ -12,34 +12,39 @@ namespace PSAutomation.Test
     [TestFixture]
     public abstract class PSTestBase
     {
-        protected static T RunCommand<T>(string command)
+        private static readonly InitialSessionState State = CreateState();
+        private static readonly Runspace DefaultRunspace = CreateRunspace();
+
+        private static InitialSessionState CreateState()
         {
             var state = InitialSessionState.CreateDefault();
             state.ImportPSModule(new[] { Path.Combine(Environment.CurrentDirectory, "PSAutomation.psd1") });
-            using (var runspace = RunspaceFactory.CreateRunspace(state))
+            return state;
+        }
+
+        private static Runspace CreateRunspace()
+        {
+            var runspace = RunspaceFactory.CreateRunspace(State);
+            runspace.Open();
+            return runspace;
+        }
+
+        protected static T RunCommand<T>(string command)
+        {
+            using (var p = DefaultRunspace.CreatePipeline(command))
             {
-                runspace.Open();
-                using (var p = runspace.CreatePipeline(command))
-                {
-                    var results = p.Invoke();
-                    Assert.AreEqual(1, results.Count);
-                    return (T)results[0].BaseObject;
-                }
+                var results = p.Invoke();
+                Assert.AreEqual(1, results.Count);
+                return (T)results[0].BaseObject;
             }
         }
 
         protected static T[] RunCommandCollection<T>(string command)
         {
-            var state = InitialSessionState.CreateDefault();
-            state.ImportPSModule(new[] { Path.Combine(Environment.CurrentDirectory, "PSAutomation.dll") });
-            using (var runspace = RunspaceFactory.CreateRunspace(state))
+            using (var p = DefaultRunspace.CreatePipeline(command))
             {
-                runspace.Open();
-                using (var p = runspace.CreatePipeline(command))
-                {
-                    var results = p.Invoke();
-                    return results.Select(r => (T)r.BaseObject).ToArray();
-                }
+                var results = p.Invoke();
+                return results.Select(r => (T)r.BaseObject).ToArray();
             }
         }
     }
