714b475280eb3444aec1ddf9df3a157d001973fa
Mike Zboray <mzboray@users.noreply.github.com>
2014-08-21 12:48:22 -0700

    use psake 4.3.2 for build

---
+++ default.ps1
@@ -0,0 +1,41 @@
+properties {
+    $binDir = "$PSScriptRoot\bin"
+	$modules = "$binDir\Modules"
+	$nunitConsole = "$PSScriptRoot\src\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe"
+	$psAutomationModule  = "$modules\PSAutomation"
+}
+
+Framework "4.0"
+
+task default -depends Test
+
+task Clean {
+    rm $binDir -Recurse -Force
+}
+
+task Init -depends Clean {
+    mkdir $binDir
+    mkdir $modules
+	mkdir $psAutomationModule
+}
+
+task Compile -depends Init {
+	msbuild "src\PSAutomation.sln" /t:Rebuild /p:OutDir="$binDir"
+	pushd $binDir
+	cp PSAutomation.dll, PSAutomation.pdb, PSAutomation.psd1, PSAutomation.psm1, PSAutomation.Types.ps1xml $psAutomationModule	
+	popd
+}
+
+task Test -depends Compile {
+    $testDlls = (ls $binDir -Filter *.Test*.dll | Convert-Path) -join " "
+    & $nunitConsole $testDlls /framework=net-4.5
+}
+
+task Run -depends Test {
+    $oldModulePath = $env:PSModulePath
+    if ($env:PSModulePath -notlike "*$modules*") {
+        $env:PSModulePath += ";$modules"
+    }
+	Start-Process -File "powershell.exe" -ArgumentList "-NoExit", "-Command & { Import-Module PSAutomation }"
+	$env:PSModulePath = $oldModulePath
+}
--- run.bat
+++
@@ -1,2 +0,0 @@
-call "%VS120ComnTools%vsvars32.bat"
-powershell.exe -File "%~dp0run.ps1"
