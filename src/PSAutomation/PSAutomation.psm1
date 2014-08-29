b72e782a580b192b9fe0e5adbd61c1452be75ce2
Mike Zboray <mzboray@users.noreply.github.com>
2014-08-28 18:39:43 -0700

    add focus changed via function

--- src/PSAutomation/PSAutomation.psm1
+++ src/PSAutomation/PSAutomation.psm1
@@ -1,3 +1,10 @@
+function Register-FocusChangedEvent {
+	param([ScriptBlock]$Action)
+
+	$evt = [PSAutomation.PSAutomationEvent]::Instance
+	Register-ObjectEvent $evt -EventName FocusChanged -SourceIdentifier "PSAutomation.FocusChanged" -Action $Action -SupportEvent | out-null
+}
+
 Set-Alias gce Get-ChildElement
 Set-Alias nc New-Condition
 Export-ModuleMember -Alias * -Function * -Cmdlet *
--- src/PSAutomation/PSAutomationEvent.cs
+++ src/PSAutomation/PSAutomationEvent.cs
@@ -1,36 +1,16 @@
-﻿using Microsoft.PowerShell.Commands;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Automation;
 
-namespace PSAutomation.Commands
+namespace PSAutomation
 {
-    [Cmdlet("Register", "FocusChangedEvent")]
-    public sealed class RegisterFocusChangedEventCommand : PSCmdlet
-    {
-        [Parameter]
-        public ScriptBlock Action { get; set; }
-
-        protected override void ProcessRecord()
-        {
-            var regCmd = new RegisterObjectEventCommand()
-            {
-                InputObject = new PSObject(PSAutomationEvent.Instance),
-                EventName = "FocusChanged",
-                Action = this.Action
-            };
-            this.WriteObject(regCmd.Invoke(), true);
-        }
-    }
-
     public class PSAutomationEvent
     {
         private static readonly PSAutomationEvent _instance = new PSAutomationEvent();
-        
+
         private PSAutomationEvent()
         {
             Automation.AddAutomationFocusChangedEventHandler(this.FocusChangedHandler);
@@ -47,6 +27,6 @@ namespace PSAutomation.Commands
             {
                 handler(sender, e);
             }
-        }        
+        }
     }
 }
